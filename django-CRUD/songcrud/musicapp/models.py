53a6ad9d66c173e28af260dfea096fe9905c4c7b
Ahmed Olawale A <ahmed4donola1@gmail.com>
2022-10-30 20:35:20 +0000

    [feat]: week 5 tasks

---
+++ django-CRUD/songcrud/musicapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.db.models import Model
+
+# Create your models here.
+class Artiste(Model):
+    first_name = models.CharField(max_length = 150)
+    last_name = models.CharField(max_length = 150)
+    age = models.IntegerField()
+
+class Song(Model):
+    title = models.CharField(max_length = 150)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+    Artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+
+class Lyrics(Model):
+    content = models.TextField()
+    song_id = models.IntegerField()
+    Song = models.ForeignKey(Song, on_delete = models.CASCADE)
